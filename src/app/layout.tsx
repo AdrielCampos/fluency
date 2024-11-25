d1880e6560b82f8fa32342f33f19b3302f2928f6
Adriel Campos <adrielcampos.dev@gmail.com>
2024-11-25 15:49:48 -0300

    Refactor layout and manifest for improved PWA support; remove install prompt component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,14 +4,14 @@ import { cn } from '@/common/utils/cn';
 import { Suspense } from 'react';
 import { UserProvider } from '@/common/providers/user-provider';
 import { Loading } from '@/common/components/loading';
-import { InstallPrompt } from '@/common/components/install-prompt';
+import Head from 'next/head';
 import '../config/styles/globals.css';
 
 const nunito = Nunito({ subsets: ['latin'], variable: '--font-nunito' });
 
 export const metadata: Metadata = {
   title: 'Fluency',
-  description: 'Fluency is a language learning platform.',
+  description: 'Aprenda inglês de forma rápida e eficiente.',
 };
 
 export const viewport: Viewport = {
@@ -27,12 +27,36 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" className="flex items-center justify-center">
+      <Head>
+        <meta name="theme-color" content="#333333" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="google" content="notranslate" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@site" />
+        <meta name="twitter:url" content="https://fluency-com.vercel.app/" />
+        <meta name="twitter:title" content="Fluency" />
+        <meta name="twitter:description" content="Aprenda inglês de forma rápida e eficiente." />
+        <meta name="twitter:image" content="https://fluency-com.vercel.app//elated_lira.jpg" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Fluency" />
+        <meta property="og:description" content="Aprenda inglês de forma rápida e eficiente." />
+        <meta property="og:site_name" content="Fluency" />
+        <meta property="og:url" content="https://fluency-com.vercel.app/" />
+        <meta property="og:image" content="https://fluency-com.vercel.app//elated_lira.jpg" />
+
+        <link rel="preload" href="/manifest.json" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="stylesheet" href="/css/native-like-experience.css" />
+        <script async src="https://unpkg.com/pwacompat" crossOrigin="anonymous"></script>
+
+        <link rel="apple-touch-icon" sizes="120x120" href="/icons/apple-touch-icon_120.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon_180.png" />
+      </Head>
       <body className={cn(nunito.variable, 'antialiased flex-1 max-w-[480px] min-h-screen')} suppressHydrationWarning>
         <Suspense fallback={<Loading />}>
-          <UserProvider>
-            <InstallPrompt />
-            {children}
-          </UserProvider>
+          <UserProvider>{children}</UserProvider>
         </Suspense>
       </body>
     </html>
